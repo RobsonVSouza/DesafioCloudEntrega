c865f8e867eca2f65d40d972e26f94a41b102b28
Robson Vieira <robson.ppmg@gmail.com>
2022-05-27 14:24:46 -0300

    Projeto com Crud

--- app/views.py
+++ app/views.py
@@ -19,11 +19,29 @@ def create(request):
         form.save()
         return redirect('home')
 
+
 def view(request, pk):
     data = {}
-    data['db'] = Cliente.objects.get(pk=pk)
+    data['db'] = Cliente.objects.get(pk=data)
     return render(request, 'view.html', data)
 
+def edit (request, pk):
+    data = {}
+    data['db'] = Cliente.objects.get(pk=pk)
+    data['form'] = ClienteForm(instance=data['db'])
+    return render(request, 'form.html', data)
 
+def update(request, pk):
+    data= {}
+    data['db'] = Cliente.objects.get(pk=pk)
+    form = ClienteForm(request.POST or None, instance=data['db'])
+    if form.is_valid():
+        form.save
+    return redirect('home')
+
+def delete(request, pk):
+    db = Cliente.objects.get(pk=pk)
+    db.delete()
+    return redirect('home')
 
 
--- desafio/urls.py
+++ desafio/urls.py
@@ -1,5 +1,4 @@
 """desafio URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
@@ -15,13 +14,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import home, form, create, view
+from app.views import home, form, create, view , edit, update, delete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('form/', form, name='form'),
     path('create/', create, name='create'),
-    path('view/<int:pk/>', view, name='view'),
+    path('view/<int:pk>/', view, name='view'),
+    path('edit/<int:pk>/', edit, name='edit'),
+    path('update/<int:pk>/', update, name='update'),
+    path('delete/<int:pk>/', delete, name='delete'),
 
 ]
+
